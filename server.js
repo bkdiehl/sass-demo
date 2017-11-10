2d810886c592d4280c13ad6ed914f5feb19291e9
bkdiehl <bkdiehl@gmail.com>
2017-11-09 17:42:26 -0700

    add node server, simplify scss files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname+'/'));
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
